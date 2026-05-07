02e12f347a59655c5a1490f79c4c137e97a61ea2
kumar harsh <hk995814@gmail.com>
2026-05-07 22:59:45 +0530

    working on application model

---
+++ BACKEND/models/application.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+
+const ApplicationSchema =  new mongoose.Schema({
+     user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+     },
+
+     job:{
+           type:mongoose.Schema.Types.ObjectId,
+           ref:"Job",
+           required:true,
+
+     },
+     status:{
+        enum:["applied", "rejected", "accepted"],
+        default: "applied",
+        type:String,
+        required:true
+     }
+},{timestamps:true,
+    versionKey:true})
+
+ export const Application = mongoose.model("Application", ApplicationSchema)
