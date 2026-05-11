a1d4280d36472f76558e27854debfae6216679da
kumar harsh <hk995814@gmail.com>
2026-05-11 23:24:49 +0530

    savedjob application

---
+++ BACKEND/models/SavedJob.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const savedJobSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const SavedJob = mongoose.model(
+  "SavedJob",
+  savedJobSchema
+);
