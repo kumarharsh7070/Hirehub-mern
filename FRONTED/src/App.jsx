c159378021f8829ac75e54672c7a3464389b421e
kumar harsh <hk995814@gmail.com>
2026-06-02 22:06:08 +0530

    Add navbar

--- FRONTED/src/App.jsx
+++ FRONTED/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,29 +10,16 @@ import Profile from "./pages/Profile";
 function App() {
   return (
     <BrowserRouter>
+
+      <Navbar />
+
       <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
